8f7aa5c5ad1d7a1225ca9f49eb2ae8542c984c28
Gusatvo Alarcón <galarcon@meraki-s.com>
2020-02-10 18:13:30 -0500

    done

---
+++ src/app/main/kitchen/orders/orders-show-inputs/orders-show-inputs.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
+
+@Component({
+  selector: 'app-orders-show-inputs',
+  templateUrl: './orders-show-inputs.component.html',
+  styles: []
+})
+export class OrdersShowInputsComponent implements OnInit {
+
+  loadingDetails = new BehaviorSubject<boolean>(false);
+  loadingDetails$ = this.loadingDetails.asObservable();
+
+  displayedColumns: string[] = ['index', 'input', 'unit', 'quantity'];
+
+  dataSource = new MatTableDataSource();
+
+  @ViewChild(MatPaginator, { static: false }) set content(paginator: MatPaginator) {
+    this.dataSource.paginator = paginator;
+  }
+
+  currentDate = Date.now();
+
+  details$: Observable<{
+    input: string;
+    unit: string;
+    quantity: number;
+  }[]>;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/main/kitchen/orders/orders.module.ts
+++ src/app/main/kitchen/orders/orders.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrdersComponent } from './orders.component';
+import { MatDividerModule, MatIconModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatDialogModule, MatOptionModule, MatAutocompleteModule, MatSnackBarModule, MatTableModule, MatPaginatorModule, MatProgressBarModule } from '@angular/material';
+import { SatDatepickerModule, SatNativeDateModule } from 'saturn-datepicker';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { OrdersShowDetailsComponent } from './orders-show-details/orders-show-details.component';
+import { OrdersShowInputsComponent } from './orders-show-inputs/orders-show-inputs.component';
 
 @NgModule({
   declarations: [
-    OrdersComponent
+    OrdersComponent,
+    OrdersShowDetailsComponent,
+    OrdersShowInputsComponent
   ],
   imports: [
-    OrdersRoutingModule
+    CommonModule,
+    OrdersRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDividerModule,
+    MatIconModule,
+    SatDatepickerModule,
+    SatNativeDateModule ,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatOptionModule,
+    MatAutocompleteModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressBarModule
   ],
   entryComponents: [
-
+    OrdersShowDetailsComponent,
+    OrdersShowInputsComponent
   ],
 
 })
